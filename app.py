9d7554b9ea2da107e2493e83a4a98ce1a092f291
mzuazolaarr <126657223+mzuazolaarr@users.noreply.github.com>
2025-07-24 14:08:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -476,4 +476,5 @@ rsu.register_callbacks(app)
 dgt3.register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))  # Usa el puerto de Render si existe
+    app.run(debug=False, host='0.0.0.0', port=port)
